58ec9d421216d651c7c82c50ad2a9ab29a87282e
avrahamm <papaabramg@gmail.com>
2024-08-29 23:28:22 +0300

    AWS ec2 with docker - adding clock_ prefix to aim resources

--- terraform/create_iam_resources.tf
+++ terraform/create_iam_resources.tf
@@ -1,12 +1,12 @@
 # # To run once to create resources.
 # # cd terraform
 # # terraform init
-# # terraform plan -target=aws_iam_role.ec2_ecr_role -target=aws_iam_policy.ecr_access_policy -target=aws_iam_role_policy_attachment.ecr_policy_attach -target=aws_iam_instance_profile.ec2_profile
-# # terraform apply -target=aws_iam_role.ec2_ecr_role -target=aws_iam_policy.ecr_access_policy -target=aws_iam_role_policy_attachment.ecr_policy_attach -target=aws_iam_instance_profile.ec2_profile
+# # terraform plan -target=aws_iam_role.clock_ec2_ecr_role -target=aws_iam_policy.clock_ecr_access_policy -target=aws_iam_role_policy_attachment.clock_ecr_policy_attach -target=aws_iam_instance_profile.clock_ec2_profile
+# # terraform apply -target=aws_iam_role.clock_ec2_ecr_role -target=aws_iam_policy.clock_ecr_access_policy -target=aws_iam_role_policy_attachment.clock_ecr_policy_attach -target=aws_iam_instance_profile.clock_ec2_profile
 #
 #
-# resource "aws_iam_role" "ec2_ecr_role" {
-#   name = "ec2_ecr_role"
+# resource "aws_iam_role" "clock_ec2_ecr_role" {
+#   name = "clock_ec2_ecr_role"
 #
 #   assume_role_policy = jsonencode({
 #     Version = "2012-10-17"
@@ -22,8 +22,8 @@
 #   })
 # }
 #
-# resource "aws_iam_policy" "ecr_access_policy" {
-#   name        = "ecr_access_policy"
+# resource "aws_iam_policy" "clock_ecr_access_policy" {
+#   name        = "clock_ecr_access_policy"
 #   path        = "/"
 #   description = "IAM policy for ECR access"
 #
@@ -50,12 +50,12 @@
 #   })
 # }
 #
-# resource "aws_iam_role_policy_attachment" "ecr_policy_attach" {
-#   role       = aws_iam_role.ec2_ecr_role.name
-#   policy_arn = aws_iam_policy.ecr_access_policy.arn
+# resource "aws_iam_role_policy_attachment" "clock_ecr_policy_attach" {
+#   role       = aws_iam_role.clock_ec2_ecr_role.name
+#   policy_arn = aws_iam_policy.clock_ecr_access_policy.arn
 # }
 #
-# resource "aws_iam_instance_profile" "ec2_profile" {
-#   name = "ec2_profile"
-#   role = aws_iam_role.ec2_ecr_role.name
+# resource "aws_iam_instance_profile" "clock_ec2_profile" {
+#   name = "clock_ec2_profile"
+#   role = aws_iam_role.clock_ec2_ecr_role.name
 # }
--- terraform/iam_resources.tf
+++ terraform/iam_resources.tf
@@ -1,35 +1,35 @@
 # Using existing iam resources
 
 # Data source for the existing IAM role
-data "aws_iam_role" "ec2_ecr_role" {
-  name = "ec2_ecr_role"
+data "aws_iam_role" "clock_ec2_ecr_role" {
+  name = "clock_ec2_ecr_role"
 }
 
 # Data source for the existing IAM policy
-data "aws_iam_policy" "ecr_access_policy" {
-  name = "ecr_access_policy"
+data "aws_iam_policy" "clock_ecr_access_policy" {
+  name = "clock_ecr_access_policy"
 }
 
 # Data source for the existing IAM instance profile
-data "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2_profile"
+data "aws_iam_instance_profile" "clock_ec2_profile" {
+  name = "clock_ec2_profile"
 }
 
 # Ensure the policy is attached to the role (this is idempotent)
-resource "aws_iam_role_policy_attachment" "ecr_policy_attach" {
-  role       = data.aws_iam_role.ec2_ecr_role.name
-  policy_arn = data.aws_iam_policy.ecr_access_policy.arn
+resource "aws_iam_role_policy_attachment" "clock_ecr_policy_attach" {
+  role       = data.aws_iam_role.clock_ec2_ecr_role.name
+  policy_arn = data.aws_iam_policy.clock_ecr_access_policy.arn
 }
 
 # Output the ARNs of the existing resources for reference
 output "role_arn" {
-  value = data.aws_iam_role.ec2_ecr_role.arn
+  value = data.aws_iam_role.clock_ec2_ecr_role.arn
 }
 
 output "policy_arn" {
-  value = data.aws_iam_policy.ecr_access_policy.arn
+  value = data.aws_iam_policy.clock_ecr_access_policy.arn
 }
 
 output "instance_profile_arn" {
-  value = data.aws_iam_instance_profile.ec2_profile.arn
+  value = data.aws_iam_instance_profile.clock_ec2_profile.arn
 }
