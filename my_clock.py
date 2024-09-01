c98ad224d567c5c489caedb19f6868c7878b7222
avrahamm <papaabramg@gmail.com>
2024-09-01 22:08:22 +0300

    AWS ec2 with docker - reverting ec2_ip, may be in future

--- my_clock.py
+++ my_clock.py
@@ -7,6 +7,12 @@ import requests
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
+def get_ec2_instance_ip():
+    # TODO!
+    response = requests.get('http://169.254.169.254/latest/meta-data/public-ipv4', timeout=2)
+    return response.text
+
+
 def get_current_time(time_format):
     current_time = datetime.datetime.now().strftime(time_format)
     return current_time
@@ -30,7 +36,6 @@ def get_ec2_instance_ip():
 
 def main():
     time_format = os.getenv('TIME_FORMAT', "%Y-%m-%d %H:%M:%S")
-    ec2_ip = get_ec2_instance_ip()
     output_file_path = os.getenv('OUTPUT_FILE_PATH', "/usr/share/nginx/html/myclock.html")
     update_clock_time_interval = int(os.getenv('UPDATE_CLOCK_TIME_INTERVAL', '10'))
 
@@ -38,8 +43,7 @@ def main():
 
     while True:
         current_time = get_current_time(time_format)        
-        output_string = (f"Current time: {current_time} - Running on EC2 IP: {ec2_ip}")
-        write_time_output(output_string, output_file_path)
+        write_time_output(current_time, output_file_path)
         time.sleep(update_clock_time_interval)
 
 
