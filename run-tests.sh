038056f982442115dd523f6187046854d5a27ca6
avrahamm <papaabramg@gmail.com>
2024-09-02 15:52:22 +0300

    AWS ec2 with docker - e2e tests return

--- run-tests.sh
+++ run-tests.sh
@@ -15,21 +15,21 @@ echo "Running Pytest tests..."
 /home/myuser/.local/bin/pytest tests/test_my_clock.py
 
 # Start Nginx
-#echo "Starting Nginx..."
-#nginx
-#
-## Start the main application in the background
-#echo "Starting the main application..."
-#python3 my_clock.py &
-#
-## Run the E2E tests
-#echo "Running E2E tests..."
-#python3 tests/test_my_clock_e2e.py
-#
-## Wait E2E tests to complete
-#sleep 30
-#
-## Stop the application and Nginx
-#echo "Stopping the application and Nginx..."
-#kill %1
-#nginx -s stop
+echo "Starting Nginx..."
+nginx
+
+# Start the main application in the background
+echo "Starting the main application..."
+python3 my_clock.py &
+
+# Run the E2E tests
+echo "Running E2E tests..."
+python3 tests/test_my_clock_e2e.py
+
+# Wait E2E tests to complete
+sleep 30
+
+# Stop the application and Nginx
+echo "Stopping the application and Nginx..."
+kill %1
+nginx -s stop
